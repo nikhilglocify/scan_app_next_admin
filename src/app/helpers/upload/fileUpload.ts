82732614cb404ec38c44f414e725cbc141544d6d
Nikhil <Nikhil@glocify.com>
2025-01-16 10:41:04 +0530

    aws-sdk-v3

--- src/app/helpers/upload/fileUpload.ts
+++ src/app/helpers/upload/fileUpload.ts
@@ -1,11 +1,21 @@
 import path from "path";
 import fs from "fs";
 import AWS from "aws-sdk";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+
 
 const s3 = new AWS.S3({
   region: process.env.AWSREGION, // e.g., "us-west-2"
   accessKeyId: process.env.AWS_ACCESS_KEYID, // Your AWS access key
   secretAccessKey: process.env.AWS_SECRET_ACCESSKEY, // Your AWS secret key
+});
+
+const s3Client = new S3Client({
+  region: process.env.AWSREGION, // e.g., "us-west-2"
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEYID!, // Your AWS access key
+    secretAccessKey: process.env.AWS_SECRET_ACCESSKEY!, // Your AWS secret key
+  },
 });
 
 export async function uploadFileToLocal(file: File, id: string) {
@@ -69,6 +79,43 @@ export async function uploadFileToLocal(file: File, id: string) {
     await s3.upload(s3Params).promise();
     console.log(`File uploaded to S3: ${s3Params.Key}`);
     return s3Params.Key
+  }
+
+
+  export async function uploadFileToAwsS3(file: File, uuid: string) {
+    console.log("Running uploadFileToS3", { file, uuid });
+  
+    const chunks: Uint8Array[] = [];
+    const readableStream = file.stream();
+    const reader = readableStream.getReader();
+    let result;
+  
+    while (!(result = await reader.read()).done) {
+      const chunk = result.value;
+      chunks.push(chunk);
+    }
+  
+    const buffer = Buffer.concat(chunks); // Combine all chunks into a buffer
+    const fileKey = generateFileKey(uuid, file.name); // Generate S3 key for the file
+  
+    const s3Params = {
+      Bucket: process.env.S3_BUCKET_NAME!, // Your S3 bucket name
+      Key: fileKey, // Key (file path in the bucket)
+      Body: buffer, // File content
+      ContentType: file.type, // MIME type of the file
+      // ACL: 'public-read', // Optional: Set file permissions
+    };
+  
+    try {
+      // Upload file to S3
+      const command = new PutObjectCommand(s3Params);
+      await s3Client.send(command);
+      console.log(`File uploaded to S3: ${s3Params.Key}`);
+      return s3Params.Key;
+    } catch (error) {
+      console.error("Error uploading file to S3:", error);
+      throw error;
+    }
   }
   
   
